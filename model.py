86f975b783fb417eb8f97354131cd18a86887b20
Roy Dale Caliwag <roydale.valerio.caliwag@gmail.com>
2026-05-22 01:16:57 +0800

    Updated model.py

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ class ChatModel(ChatOpenAI):
             **kwargs
         )
 
-def get_model(model_name: str = "<default model>") -> ChatModel:
+def get_model(model_name: str = "openai/gpt-oss-20b:free") -> ChatModel:
     """
     Gets a reference to a model
     
@@ -39,36 +39,36 @@ def get_model(model_name: str = "<default model>") -> ChatModel:
 
 if __name__ == "__main__":
 # when run as a script, run some tests to demonstrate capabilities
-#    model = get_model()
+    model = get_model()
 #    from langchain_core.messages import HumanMessage
-#    from langchain.prompts import ChatPromptTemplate
+    from langchain.prompts import ChatPromptTemplate
 
-#    ???
-#    ???
-#    ???
-#    ???
+    prompt_template = ChatPromptTemplate([
+        ("human", "You are a helpful assistant."),
+        ("human", "What is {playwright}'s most recent play?")
+    ])
 
 #    response = model.invoke(
-#        [???("You are a helpful assistant."),
-#         ???("What are some plays by Tawfiq al-Hakim?")])
+#        [HumanMessage("You are a helpful assistant."),
+#         HumanMessage("What are some plays by Tawfiq al-Hakim?")])
 #    print(response.content)
 #    print("----------")
 #    response = model.invoke(
-#        [???("You are a helpful assistant."),
-#         ???("What is Ryan Calais Camerons's most recent play?")])
+#        [HumanMessage("You are a helpful assistant."),
+#         HumanMessage("What is Ryan Calais Camerons's most recent play?")])
 #    print(response.content)
 #    print("----------")
 #    response = model.invoke(
-#        [???("You are a helpful assistant."),
-#         ???("What Broadway shows have more than 10,000 performances?")])
+#        [HumanMessage("You are a helpful assistant."),
+#         HumanMessage("What Broadway shows have more than 10,000 performances?")])
 #    print(response.content)
 
 #    print(prompt_template.invoke({"playwright": "Ryan Calais Cameron"}))
 #    response = model.invoke(prompt_template.invoke({"playwright": "Ryan Calais Cameron"}))
 #    print(response.content)
 
-#    chain = ???
-#    response = ???{"playwright": "Ryan Calais Cameron"})
-#    print(response.content)
+    chain = prompt_template | model
+    response = chain.invoke({"playwright": "Ryan Calais Cameron"})
+    print(response.content)
 
     pass
